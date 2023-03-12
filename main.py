76ef7d93dfb77c897fdd459f449196d7c928b11c
Joseph Curtis <jcur175@wgu.edu>
2023-03-12 16:59:33 -0700

    Initialize project

---
+++ main.py
@@ -0,0 +1,29 @@
+__license__ = "BSD 4-Clause"
+__copyright__ = """
+
+ Copyright 2023 Joseph Curtis
+ 
+ Licensed under the BSD 4-Clause License, (the “Original” or “Old” License);
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+  https://choosealicense.com/licenses/bsd-4-clause/
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License for the specific language governing permissions and limitations under
+ the License.
+
+"""
+__author__ = "Joseph Curtis"
+__version__ = "2023.03.12"
+# The main class for the Daily Local Deliveries Route Planner
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
