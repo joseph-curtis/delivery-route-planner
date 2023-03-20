f222e883693c47fabde223718ea9d2a9a433d3fa
Joseph Curtis <jcur175@wgu.edu>
2023-03-20 09:01:47 -0700

    include data files and command args to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 __author__ = "Joseph Curtis"
 __license__ = "BSD 4-Clause"
-__copyright__ = """
-
- Copyright 2023 Joseph Curtis
+__copyright__ = """Copyright 2023 Joseph Curtis 
  
  Licensed under the BSD 4-Clause License, (the “Original” or “Old” License);
  you may not use this file except in compliance with the License.
@@ -20,17 +18,33 @@ __copyright__ = """
  CITATION.cff file.
 
 """
+
 # Title: Daily Local Delivery Route Planner Application
-# Description: Determines an efficient route and delivery distribution for Daily Local Deliveries (DLD)
-# Date: 14 Mar 2023
-# Sources: w3schools.com/python, docs.python.org, sdf stackoverflow.com, sololearn.com,
-#           Lysecky, R., & Vahid, F. (2018, June). C950: Data Structures and Algorithms II. zyBooks.
+# Description: Determines an efficient route and delivery distribution
+#              for Daily Local Deliveries (DLD)
+# Date: 19 Mar 2023
+# Sources: w3schools.com/python, docs.python.org, geeksforgeeks.org,
+#          stackoverflow.com, sololearn.com,
+#          Lysecky, R., & Vahid, F. (2018, June).
+#          C950: Data Structures and Algorithms II. zyBooks.
 #           [https://learn.zybooks.com/zybook/WGUC950AY20182019]
+import csv
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Process Daily Local Deliveries.')
+parser.add_argument('--table', '-t', required=False, default='data/distance-table.csv',
+                    help='The file used for the distance table that describes the distances to each node')
+parser.add_argument('--packages', '-p', required=False, default='data/package-file.csv',
+                    help='The file that includes all packages that will be delivered in the same day.')
+args = parser.parse_args()
 
 
 def main():
     """The entry point for the Daily Local Delivery Route Planner Application"""
-    pass
+    with open(args.table, 'r') as d_table:
+        data = list(csv.reader(d_table, delimiter=','))
+        for row in data[1:]:
+            print(row)
 
 
 if __name__ == '__main__':
