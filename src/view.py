895fb8a2f1034d68b7ca863aba22e39671fc6954
Joseph Curtis <jcur175@wgu.edu>
2023-04-05 16:32:31 -0700

    added main menu

--- src/view.py
+++ src/view.py
@@ -1,8 +1,6 @@
 __author__ = "Joseph Curtis"
 __license__ = "BSD 4-Clause"
-__copyright__ = """
-
- Copyright 2023 Joseph Curtis
+__copyright__ = """Copyright 2023 Joseph Curtis 
 
  Licensed under the BSD 4-Clause License, (the “Original” or “Old” License);
  you may not use this file except in compliance with the License.
@@ -20,5 +18,33 @@ __copyright__ = """
  CITATION.cff file.
 
 """
-# Date: 14 Mar 2023
 
+import sys
+
+
+# Date: 28 Mar 2023
+
+def main_menu():
+    print("\nWelcome to WGUPS Package Delivery Route Planner")
+
+    user_quits = False
+    while not user_quits:
+        print("\n************************************************************\n")
+        print("\nPlease select option from below:")
+        print(" [1] Show All Packages and Total Mileage at End of Day")
+        print(" [2] Get a Package Status from Time")
+        print(" [3] Show All Packages Status from Time")
+        print(" [4] Exit Program")
+        print("\n************************************************************\n")
+        option = input("Chose an option [1],[2],[3], or [4] : ")
+        if option == "1":
+            pass
+        elif option == "2":
+            pass
+        elif option == "3":
+            pass
+        elif option == "4":
+            user_quits = True
+            sys.exit("Exiting Application. Have a nice day!")
+        else:
+            print("Wrong option, please try again!")
